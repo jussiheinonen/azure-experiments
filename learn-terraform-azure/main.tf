8d471fa39539b8e9fdb5e6f7787056ee19b3464e
Jussi Heinonen <heinonen.jussi@gmail.com>
2021-08-18 16:47:15 +0100

    Adding VNET into deployment and linking it with rg resource group

--- learn-terraform-azure/main.tf
+++ learn-terraform-azure/main.tf
@@ -17,4 +17,17 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = "uksouth"
+  tags     = {
+    environment = "Development and learning"
+    team        = "one person team"
+  }
+
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+    name                = "myTFVnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "uksouth"
+    resource_group_name = azurerm_resource_group.rg.name
 }
